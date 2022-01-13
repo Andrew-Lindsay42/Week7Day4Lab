dba4c829fab1c64fea583055887db4fe27781f80
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-01-13 14:54:38 +0000

    Fetch now works :)

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -7,35 +7,39 @@ import TopStoriesList from '../components/TopStoriesList';
 const NewsContainer = () => {
 
     const [urlList, seturlList] = useState([]);
+    const [storyList, setStoryList] = useState([]);
 
     const getAllURLS = function(){
         fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
         .then(response => response.json())
-        .then(urls => seturlList(urls))}
+        .then(urls => seturlList(urls))
+    };
+
+    const getStories = function(result){
+
+        let tempList = []
+
+        Promise.all(
+            result.map(url => fetch(`https://hacker-news.firebaseio.com/v0/item/${url}.json`).then(response => response.json())
+            .then(value => tempList.push(value))
+            .then(() => setStoryList(tempList))))
+    }
 
     useEffect(() => {
         getAllURLS();
-        getDetails(urlList);
-      }, [])
+    }, [])
 
-    const getDetails = function(urlList){
-        let storyList = []
-        for (let url of urlList){
-            let promise = fetch(`https://hacker-news.firebaseio.com/v0/item/${url}.json`).then(response => response.json)
-            storyList.push(promise)
-        }
-        
-        Promise.all(storyList)
-        .then(values => console.log(values))
-    }
-
+    useEffect(() => {
+        let result = urlList.slice(0,10);
+        getStories(result);
+    }, [urlList])
 
     return(
         <>
         <h1>This is the News Container</h1>
         {/* <Header/>
         <FilterBox/>
-        <TopStoriesList/>
+        <TopStoriesList storyList={storylist}/>
         <Footer/> */}
         </>
     )
